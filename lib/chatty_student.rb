62d2b5b52572b6c82c67cd3172ef03099264e334
Ted Sazon <tedsazon@gmail.com>
2019-01-09 03:30:13 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,15 +5,7 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
+    super*10
+    
   end
 end
